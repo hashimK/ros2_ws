2b50e3508fa0d0451998a0a2d3edd073f94cb957
Hashim Kahily <kahilyhashim10@gmail.com>
2021-10-17 08:03:42 +0400

    optimized data publishing to cloud

--- src/secured_cloud_drone_pkg/secured_cloud_drone_pkg/cloud_informer.py
+++ src/secured_cloud_drone_pkg/secured_cloud_drone_pkg/cloud_informer.py
@@ -24,30 +24,43 @@ class CloudInformer(Node):
         self.amsl = 0.0
         self.groundspeed = 0.0
         self.yaw = 0.0
-        self.subscription_started = False
+        # self.subscription_started = False
+
+        self.prev_latitude = 0.0
+        self.prev_longitude = 0.0
+        self.prev_amsl = 0.0
+        self.prev_groundspeed = 0.0
+        self.prev_yaw = 0.0
 
         self.subscriber_ = self.create_subscription(Twist,"cloud_telemetry",self.callback_cloud_telemetry,10)
-        self.timer_ = self.create_timer(2,self.publish_to_cloud_continously) # publish data to cloud every 2 seconds
+        self.timer_ = self.create_timer(0.5,self.publish_to_cloud_continously) # publish data to cloud every 500 ms
         
     def callback_cloud_telemetry(self, msg):
         self.latitude = msg.linear.x
         self.longitude = msg.linear.y
-        self.amsl = msg.linear.z
-        self.groundspeed = msg.angular.x
-        self.yaw = msg.angular.y
-        self.subscription_started = True
+        self.amsl = round(msg.linear.z,1)
+        self.groundspeed = round(msg.angular.x,1)
+        self.yaw = round(msg.angular.y)
+        # self.subscription_started = True
 
     def publish_to_cloud_continously(self):
-        if self.subscription_started:
+        # if self.subscription_started:
+        if ((self.prev_latitude!=self.latitude) or (self.prev_longitude!=self.longitude) or (self.prev_amsl!=self.amsl) or (self.prev_groundspeed!=self.groundspeed) or (self.prev_yaw!=self.yaw)):
             self.ts = time.time()
             self.telemetryCollectionRef.document(str(int(self.ts))).set({
                 u'id' :int(self.ts),
                 u'gps_location': firebase_admin.firestore.GeoPoint(self.latitude, self.longitude),
-                u'altitude_amsl': round(self.amsl,1),
-                u'groundspeed': round(self.groundspeed,1),
-                u'yaw': round(self.yaw,1),
+                u'altitude_amsl': self.amsl,
+                u'groundspeed': self.groundspeed,
+                u'yaw': self.yaw,
                 u'timestamp': datetime.datetime.now()
             })
+            self.prev_latitude = self.latitude
+            self.prev_longitude = self.longitude
+            self.prev_amsl = self.amsl
+            self.prev_groundspeed = self.groundspeed
+            self.prev_yaw = self.yaw
+
 
 
 
